b1b8cba2c292bf695ad1f36d362c67403b1a7b1f
kskotenko <kskotenko@gmail.com>
2022-07-25 14:53:08 +0300

    update time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+	let date = new Date(timestamp)
+	let hours = date.getHours()
+	let minutes = date.getMinutes()
+	let day = date.getDay()
+	if (minutes < 10) {
+		minutes = `0${minutes}`
+	}
+	if (hours < 10) {
+		hours = `0${hours}`
+	}
+	let days = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Sunday',
+	]
+	return `${days[day]} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
 	console.log(response)
 	let temperature_elem = document.querySelector('#temperature')
@@ -5,11 +28,13 @@ function displayTemperature(response) {
 	let description_elem = document.querySelector('#description')
 	let humidity_elem = document.querySelector('#humidity')
 	let wind_elem = document.querySelector('#wind')
+	let date_elem = document.querySelector('#date')
 	temperature_elem.innerHTML = Math.round(response.data.main.temp)
 	city_elem.innerHTML = response.data.name
 	description_elem.innerHTML = response.data.weather[0].description
 	humidity_elem.innerHTML = response.data.main.humidity
 	wind_elem.innerHTML = Math.round(response.data.wind.speed)
+	date_elem.innerHTML = formatDate(response.data.dt * 1000)
 }
 
 let api_key = '63608bc5eef30d17258d77a3cb58927f'
